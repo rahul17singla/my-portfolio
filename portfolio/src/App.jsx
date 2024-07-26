ad2fb6e78df95fd6521187896ec313f3fdf62a3b
Rahul Singla <72143643+rahul17singla@users.noreply.github.com>
2024-07-26 19:22:40 +0530

    Update App.jsx
    added loading

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -7,8 +7,23 @@ import ProjectView from "./pages/ProjectView";
 import Footer from "./pages/miniComponents/Footer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+      </div>
+    );
+  }
+  
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Router>
